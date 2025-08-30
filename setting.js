430c09452dcd28790a1c30f559ac314e074c20ce
PASIYAv12 <pasidusampath280@gmail.com>
2025-08-30 06:40:56 +0530

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,8 @@
+module.exports = {
+  prefix: '.',              // command prefix, change as you like
+  ownerNumber: '94712345678', // bot owner WhatsApp number (optional)
+  botName: 'PASIYA-MD',     // bot display name
+  sessionId: 'session',     // session id file name
+  autoTyping: true,         // auto typing status (true/false)
+  autoRead: true,           // auto read messages (true/false)
+};
